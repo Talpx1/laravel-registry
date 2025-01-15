7c1a934e2ab1151ba5c254f3a43eb2e74b1ca974
Talpx1 <simo3401@gmail.com>
2025-01-15 22:23:28 +0100

    added ability to collect as collectiona nd array enum case names

--- src/Enums/Contracts/CanBeCollected.php
+++ src/Enums/Contracts/CanBeCollected.php
@@ -11,6 +11,14 @@ use Illuminate\Support\Collection;
  */
 interface CanBeCollected {
     /**
+     * Returns an array with the cases name as keys and the cases value as values for backed enums.
+     * For pure enums, it will return an array of case names.
+     *
+     * @return array<string, T>
+     */
+    public static function toArray(): array;
+
+    /**
      * Get the values of a backed enum as an array.
      *
      * @return T[]
@@ -23,4 +31,18 @@ interface CanBeCollected {
      * @return Collection<int, T>
      */
     public static function values(): Collection;
+
+    /**
+     * Get the case names of a backed enum as an array.
+     *
+     * @return T[]
+     */
+    public static function caseNamesToArray(): array;
+
+    /**
+     * Get the case names of a backed enum as a collection.
+     *
+     * @return Collection<int, T>
+     */
+    public static function caseNames(): Collection;
 }
--- src/Enums/Traits/CanBeCollected.php
+++ src/Enums/Traits/CanBeCollected.php
@@ -2,23 +2,66 @@
 
 namespace Talp1\LaravelRegistry\Enums\Traits;
 
+use BackedEnum;
 use Illuminate\Support\Collection;
 
 /**
- * @template T
+ * @template TValue
  */
 trait CanBeCollected {
     /**
-     * @return T[]
+     * Returns an array with the cases name as keys and the cases value as values for backed enums.
+     * For pure enums, it will return an array of case names.
+     *
+     * @return array<string, TValue>
+     */
+    public static function toArray(): array {
+        $array = [];
+
+        if (! is_a(static::class, BackedEnum::class, true)) {
+            return static::caseNamesToArray();
+        }
+
+        foreach (static::cases() as $case) {
+            $array[$case->name] = $case->value;
+        }
+
+        return $array;
+    }
+
+    /**
+     * Get the values as an array.
+     *
+     * @return TValue[]
      */
     public static function valuesToArray(): array {
         return array_map(fn ($case) => $case->value, static::cases());
     }
 
     /**
-     * @return Collection<int, T>
+     * Get the values as a collection.
+     *
+     * @return Collection<int, TValue>
      */
     public static function values(): Collection {
         return collect(static::valuesToArray());
+    }
+
+    /**
+     * Get the case names as an array.
+     *
+     * @return string[]
+     */
+    public static function caseNamesToArray(): array {
+        return array_map(fn ($case) => $case->name, static::cases());
+    }
+
+    /**
+     * Get the case names as a collection.
+     *
+     * @return Collection<int, string>
+     */
+    public static function caseNames(): Collection {
+        return collect(static::caseNamesToArray());
     }
 }
