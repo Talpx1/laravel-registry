3e0593feb3dc2170e47736f2c7db711cb1b95a4a
Talpx1 <simo3401@gmail.com>
2025-01-16 01:37:07 +0100

    added arch testing

--- tests/ArchTest.php
+++ tests/ArchTest.php
@@ -3,3 +3,25 @@
 arch('it will not use debugging functions')
     ->expect(['dd', 'dump', 'ray'])
     ->each->not->toBeUsed();
+
+arch('models are classes')
+    ->expect('Talp1\LaravelRegistry\Models')
+    ->toBeClasses()
+    ->ignoring('Talp1\LaravelRegistry\Models\Traits');
+
+arch('model traits are traits')
+    ->expect('Talp1\LaravelRegistry\Models\Traits')
+    ->toBeTraits();
+
+arch('enums are enums')
+    ->expect('Talp1\LaravelRegistry\Enums')
+    ->toBeEnums()
+    ->ignoring(['Talp1\LaravelRegistry\Enums\Traits', 'Talp1\LaravelRegistry\Enums\Contracts']);
+
+arch('enums traits are traits')
+    ->expect('Talp1\LaravelRegistry\Enums\Traits')
+    ->toBeTraits();
+
+arch('enums contracts are interfaces')
+    ->expect('Talp1\LaravelRegistry\Enums\Contracts')
+    ->toBeInterface();
