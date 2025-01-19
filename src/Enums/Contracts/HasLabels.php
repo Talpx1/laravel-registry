1bf2d473bbc835b22e62b32d0fe44afb0a7366a3
Talpx1 <simo3401@gmail.com>
2025-01-20 00:37:03 +0100

    added doc to enum label method

--- src/Enums/Contracts/HasLabels.php
+++ src/Enums/Contracts/HasLabels.php
@@ -3,5 +3,8 @@
 namespace Talp1\LaravelRegistry\Enums\Contracts;
 
 interface HasLabels {
+    /**
+     * Returns a label for the given case
+     */
     public function label(): string;
 }
--- src/Enums/Traits/ConvertsValueToLabel.php
+++ src/Enums/Traits/ConvertsValueToLabel.php
@@ -3,6 +3,9 @@
 namespace Talp1\LaravelRegistry\Enums\Traits;
 
 trait ConvertsValueToLabel {
+    /**
+     * Returns a label for the given case
+     */
     public function label(): string {
         return ucwords($this->value);
     }
