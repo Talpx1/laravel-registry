6d7daaf72b776914d3f79c81dae31b9d96b50d7d
Talpx1 <simo3401@gmail.com>
2025-01-26 18:06:08 +0100

    feat: added random methods to Languages enum

--- src/Enums/Languages.php
+++ src/Enums/Languages.php
@@ -6,8 +6,10 @@ namespace Talp1\LaravelRegistry\Enums;
 
 use Talp1\LaravelRegistry\Enums\Contracts\CanBeCollected;
 use Talp1\LaravelRegistry\Enums\Contracts\HasLabels;
+use Talp1\LaravelRegistry\Enums\Contracts\HasRandom;
 use Talp1\LaravelRegistry\Enums\Traits\CanBeCollected as CanBeCollectedTrait;
 use Talp1\LaravelRegistry\Enums\Traits\ConvertsValueToLabel;
+use Talp1\LaravelRegistry\Enums\Traits\HasRandom as HasRandomTrait;
 
 /**
  * Enum containing all the world spoken languages and their ISO 639 info ({@link https://en.wikipedia.org/wiki/ISO_639}).
@@ -16,10 +18,14 @@ use Talp1\LaravelRegistry\Enums\Traits\ConvertsValueToLabel;
  * If you find an error or an inconsistency, please open an issue or a pull request at {@link https://github.com/Talpx1/laravel-registry}
  *
  * @implements CanBeCollected<string>
+ * @implements HasRandom<string>
  */
-enum Languages: string implements CanBeCollected, HasLabels {
-    /** @use CanBeCollectedTrait<string> */
-    use CanBeCollectedTrait,ConvertsValueToLabel;
+enum Languages: string implements CanBeCollected, HasLabels, HasRandom {
+    /**
+     * @use CanBeCollectedTrait<string>
+     * @use HasRandomTrait<string>
+     */
+    use CanBeCollectedTrait,ConvertsValueToLabel, HasRandomTrait;
 
     case AFRIKAANS = 'afrikaans';
     case ALBANIAN = 'albanian';
