b802267ec50389ce907c430c5d90141def362cef
Talpx1 <simo3401@gmail.com>
2025-01-26 18:02:04 +0100

    chore(rector): rector configs

--- rector.php
+++ rector.php
@@ -3,7 +3,6 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\TypeDeclaration\Rector\StmtsAwareInterface\DeclareStrictTypesRector;
 
 return RectorConfig::configure()
     ->withPaths([
@@ -15,19 +14,13 @@ return RectorConfig::configure()
     // uncomment to reach your current PHP version
     // ->withPhpSets()
     // ->withTypeCoverageLevel(0)
-    // ->withPreparedSets(
-    //     deadCode: true,
-    //     codeQuality: true,
-    //     typeDeclarations: true,
-    //     privatization: true,
-    //     instanceOf: true,
-    //     earlyReturn: true,
-    //     strictBooleans: true,
-    //     carbon: true,
-    //     rectorPreset: true,
-    //     phpunitCodeQuality: false,
-    //     phpunit: false
-    // );
-    ->withRules([
-        DeclareStrictTypesRector::class,
-    ]);
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        typeDeclarations: true,
+        privatization: true,
+        instanceOf: true,
+        earlyReturn: true,
+        strictBooleans: true,
+        rectorPreset: true,
+    );
