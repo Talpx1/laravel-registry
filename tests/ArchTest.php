aff517a291684a61973087020d9f537678775e0f
Talpx1 <simo3401@gmail.com>
2025-04-04 00:03:53 +0200

    test: added 'var_dump', 'print_r' not to be used expectation to arch test

--- tests/ArchTest.php
+++ tests/ArchTest.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 arch('it will not use debugging functions')
-    ->expect(['dd', 'dump', 'ray'])
+    ->expect(['dd', 'dump', 'ray', 'var_dump', 'print_r'])
     ->each->not->toBeUsed();
 
 arch('models are classes')
