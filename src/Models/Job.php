c247086ce229bcbb6166b7ccb5e5cfb0e1a6dd80
Talpx1 <simo3401@gmail.com>
2025-04-06 17:04:03 +0200

    feat: added enum mappings to Job model

--- src/Models/Job.php
+++ src/Models/Job.php
@@ -6,5 +6,20 @@ namespace Talp1\LaravelRegistry\Models;
 
 use Talp1\LaravelRegistry\Models\Contracts\EnumModel;
 
-// TODO: test
-class Job extends EnumModel {}
+/**
+ * @extends EnumModel<\Talp1\LaravelRegistry\Enums\Jobs>
+ *
+ * @property-read string $label
+ */
+class Job extends EnumModel {
+    /**
+     * @param  \Talp1\LaravelRegistry\Enums\Jobs  $case
+     * @return array{id: string, label: string}
+     */
+    protected function enumMappings(\BackedEnum $case): array {
+        return [
+            'id' => $case->value,
+            'label' => $case->label(),
+        ];
+    }
+}
